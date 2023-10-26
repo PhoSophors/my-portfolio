2e4a0e148582aa759600cba83a9d2bb565614624
SoPhors <91106971+PhoSophors@users.noreply.github.com>
2023-10-26 16:49:33 +0700

    complate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer () {
   return (
     <>
-      <footer className="max:w-screen-full rounded-none shadow m-4 ">
+      <footer className="max:w-screen-full mt-20 rounded-none shadow m-4 ">
           <div className=" mx-auto max-w-screen-full text-center p-4">
             <span className="project-des text-sm text-gray-500 sm:text-center dark:text-gray-400">© Designed & Built by <a href="#" className="hover:underline text-white">PHO SOPHORS</a>
           </span>
